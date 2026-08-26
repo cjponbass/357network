c6f6711862351c0332a20f38b5b765c37ae8c27f
cjponbass <cjponbass@outlook.com>
2026-08-26 04:48:09 -0300

    Centralize deployment readiness gates

---
+++ src/lib/system/readiness.ts
@@ -0,0 +1,35 @@
+export type ReadinessInputs = {
+  supabaseServer: boolean;
+  supabaseServerReachable: boolean;
+  criticalSchemaReady: boolean;
+  candidateDocumentsBucketReady: boolean;
+  supabaseClient: boolean;
+  aiConfigured: boolean;
+  browserProviderExecutable: boolean;
+  submitEnabled: boolean;
+};
+
+export type ReadinessGates = {
+  readyForManualUse: boolean;
+  readyForAiPreparation: boolean;
+  readyForAutomationDryRun: boolean;
+  readyForVerifiedSubmission: boolean;
+};
+
+export function deriveReadinessGates(input: ReadinessInputs): ReadinessGates {
+  const dataPlaneReady =
+    input.supabaseServer &&
+    input.supabaseServerReachable &&
+    input.criticalSchemaReady &&
+    input.supabaseClient;
+
+  const automationDataReady = dataPlaneReady && input.candidateDocumentsBucketReady;
+
+  return {
+    readyForManualUse: dataPlaneReady,
+    readyForAiPreparation: dataPlaneReady && input.aiConfigured,
+    readyForAutomationDryRun: automationDataReady && input.browserProviderExecutable,
+    readyForVerifiedSubmission:
+      automationDataReady && input.browserProviderExecutable && input.submitEnabled,
+  };
+}
